ead48af969095d09ddf9271b0d342101db0b85fd
Abhishek Jaiswar <93974856+jaiswarabhishek@users.noreply.github.com>
2023-08-11 11:29:11 +0530

    Update baseUrl.js

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,7 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:4000"
-    : "https://memories-app-api.onrender.com";
+    : "https://memories-app-api-c9f2.onrender.com";
 
 export default baseUrl;
 
